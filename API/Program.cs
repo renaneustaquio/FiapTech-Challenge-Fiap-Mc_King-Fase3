77b5ab7ed0691824a85ef6f270b7c1e27927e786
Renan Martins <renan.ecm@hotmail.com>
2025-03-24 23:20:59 -0300

    Corrige-cognito (#31)
    * Adiciona Cognito
    
    * Adiciona Cognito (#27)
    
    * Adiciona Cognito (#30)

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,4 @@
 using Amazon.CognitoIdentityProvider;
-using Amazon.Runtime;
 using CasosDeUso;
 using Entidades.Util;
 using Infra;
@@ -15,16 +14,10 @@ builder.Logging.AddConsole();
 
 builder.WebHost.UseUrls("http://0.0.0.0:8080");
 
-var awsConfig = builder.Configuration.GetSection("AWS");
-var accessKey = awsConfig["AccessKey"];
-var secretKey = awsConfig["SecretKey"];
-var sessionToken = awsConfig["SessionToken"];
-var region = awsConfig["Region"];
-
-var credentials = new SessionAWSCredentials(accessKey, secretKey, sessionToken);
+var region = builder.Configuration["AWS:Region"];
 
 builder.Services.AddSingleton<IAmazonCognitoIdentityProvider>(
-    new AmazonCognitoIdentityProviderClient(credentials, Amazon.RegionEndpoint.GetBySystemName(region))
+    new AmazonCognitoIdentityProviderClient(Amazon.RegionEndpoint.GetBySystemName(region))
 );
 
 builder.Services.AddCors(options =>
@@ -89,7 +82,7 @@ app.UseExceptionHandler(errorApp =>
         {
             var errorDetails = new
             {
-                Title = "Regra de Negócio",
+                Title = "Regra de NegÃ³cio",
                 ex.Message,
             };
 
@@ -99,4 +92,3 @@ app.UseExceptionHandler(errorApp =>
 });
 
 app.Run();
-
